d43778276b774cf5e69169e451cda3c78142a534
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-08-20 15:59:42 +0600

    last-commit

--- withdraw.js
+++ withdraw.js
@@ -9,11 +9,19 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const previousWithdrawTotallString=WithdrawTotallElement.innerText;
     const previousWithdrawTotall=parseFloat(previousWithdrawTotallString);
 
-
+    //calculate total Previous amount
     const currentWithdrawTotall=previousWithdrawTotall+newWithdrawAmount;
 
      WithdrawTotallElement.innerText=currentWithdrawTotall;
 
     withDrawField.value='';
 
+  //get the balance total
+  const balanceTotallElement=document.getElementById('balance-totall');
+  const previousBalanceTotallString=balanceTotallElement.innerText;
+  const previousBalanceTotal=parseFloat(previousBalanceTotallString);
+
+  //calculate New balance Totall
+const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
+balanceTotallElement.innerText=newBalanceTotal;
 })
